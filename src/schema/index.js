7d6670aa46f0b39a845ca0b8257dc613c1a1c926
ofu997 <ofu997@gmail.com>
2020-06-08 23:50:59 -0700

    split schema into user and message files

--- src/schema/index.js
+++ src/schema/index.js
@@ -1,28 +1,44 @@
 import { gql } from 'apollo-server-express';
+import messageSchema from './message';
+import userSchema from './user';
 
-export default gql`
-type Query {
-  users: [User!]
-  user(id: ID!): User
-  me: User
-  messages: [Message!]!
-  message(id: ID!): Message!
-}
 
-type Mutation {
-  createMessage(text: String!): Message!
-  deleteMessage(id: ID!): Boolean!
-}
-
-type User {
-  id: ID!
-  username: String!
-  messages: [Message!]
-}
-
-type Message {
-  id: ID!
-  text: String!
-  user: User!
-}
+const linkSchema = gql`
+  type Query {
+    _: Boolean
+  }
+  type Mutation {
+    _: Boolean
+  }
+  type Subscription {
+    _: Boolean
+  }
 `;
+export default [linkSchema, userSchema, messageSchema];
+
+// export default gql`
+// type Query {
+//   users: [User!]
+//   user(id: ID!): User
+//   me: User
+//   messages: [Message!]!
+//   message(id: ID!): Message!
+// }
+
+// type Mutation {
+//   createMessage(text: String!): Message!
+//   deleteMessage(id: ID!): Boolean!
+// }
+
+// type User {
+//   id: ID!
+//   username: String!
+//   messages: [Message!]
+// }
+
+// type Message {
+//   id: ID!
+//   text: String!
+//   user: User!
+// }
+// `;
